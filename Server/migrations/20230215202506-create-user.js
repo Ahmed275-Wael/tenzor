e2a21d97e1c5b9544ab30f69a958895efa8e0256
Mazen Samer Abdallah <mazen.samer.2000@gmail.com>
2023-03-04 23:18:12 +0200

    bug fix

--- Server/migrations/20230215202506-create-user.js
+++ Server/migrations/20230215202506-create-user.js
@@ -1,53 +1,53 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Users', {
+    await queryInterface.createTable("Users", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       firstName: {
         type: Sequelize.STRING,
-        allowNull:false
+        allowNull: false,
       },
       lastName: {
         type: Sequelize.STRING,
-        allowNull:false
+        allowNull: false,
       },
       email: {
         type: Sequelize.STRING,
-        allowNull:false,
-        unique:true
+        allowNull: false,
+        unique: true,
       },
       password: {
         type: Sequelize.STRING,
-        allowNull:false
+        allowNull: false,
       },
       gender: {
         type: Sequelize.STRING,
-        allowNull:false
+        allowNull: false,
       },
       phoneNumber: {
         type: Sequelize.STRING,
-        allowNull:false
+        allowNull: false,
       },
       image: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Users');
-  }
+    await queryInterface.dropTable("Users");
+  },
 };
