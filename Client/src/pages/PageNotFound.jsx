b98fa5e1efed58f32285349ddcd1129e75afe9c0
Mazen Samer Abdallah <mazen.samer.2000@gmail.com>
2023-03-09 00:31:19 +0200

    page not found

--- Client/src/pages/PageNotFound.jsx
+++ Client/src/pages/PageNotFound.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import "./page-style/pagenotfound.css";
 
 export default function PageNotFound() {
   return (
     <>
       <Navbar />
-      Page not found ya kosomak
+      <div className="err404">
+        <h1 className="pagenotfound-text">Page not found</h1>
+      </div>
       <Footer />
     </>
   );
