bf5616ecd48d31a67e5556519c4ff7b50adaa952
mostafa <sasa1x20001@gmail.com>
2023-03-11 22:42:37 +0200

    DeleteOld

--- routes/productRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const productController = require("../controllers/productController");
-
-router.get("/", productController.getAllProducts);
-router.get("/count", productController.getProductsCount);
-router.post("/",productController.createProduct)
-module.exports = router;
