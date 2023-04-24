8b541f954ed3ae23d34ab969d8511fa1388a5887
Mazen Samer Abdallah <mazen.samer.2000@gmail.com>
2023-04-24 14:04:19 +0200

    last commit :3

--- Client/src/components/Landing.jsx
+++ Client/src/components/Landing.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import "./styles/landing.css";
+import phone1 from "../assets/images/phone1.png";
+import phone2 from "../assets/images/phone2.png";
 
 export default function Landing() {
   return (
     <div className="landing">
       <div className="content">
-        <h1>Buy</h1>
-        <h1>Sell</h1>
-        <p>And browse many different phones.</p>
+        <h1>Tenzor</h1>
+        <p>First website to trade mobile phones</p>
+      </div>
+      <div className="phone-imgs">
+        <img id="phone1" src={phone1} alt="phone1" />
+        <img id="phone2" src={phone2} alt="phone2" />
       </div>
     </div>
   );
